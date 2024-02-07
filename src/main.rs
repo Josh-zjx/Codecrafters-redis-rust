e21ed532ebf966bad0499f63f1d163ea44ed1241
Jiaxu Zhu <Josh Zhu>
2024-02-07 10:53:59 +0800

    Respond to PING

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::net::TcpListener;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -7,12 +8,20 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").expect("Listen to 6379 ports");
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("Accepting connection");
-            }
+            Ok(stream) => handle_client(stream),
             Err(_e) => {
                 println!("Error");
             }
         }
+    }
+}
+
+fn handle_client(mut stream: TcpStream) {
+    let mut read_buf: [u8; 256] = [0; 256];
+    loop {
+        let _read_result = stream.read(&mut read_buf).expect("Cannot read from stream");
+        let _write_result = stream
+            .write_all(b"+PONG\r\n")
+            .expect("Cannot write to the stream");
     }
 }
