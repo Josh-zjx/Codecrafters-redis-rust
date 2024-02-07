671c3f97fcc0e17195718d20ead8df27d1439cb4
Jiaxu Zhu <Josh Zhu>
2024-02-07 10:18:26 +0800

    listen to local port

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,18 @@
-// Uncomment this block to pass the first stage
-// use std::net::TcpListener;
+use std::net::TcpListener;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-    //
-    // let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    //
-    // for stream in listener.incoming() {
-    //     match stream {
-    //         Ok(_stream) => {
-    //             println!("accepted new connection");
-    //         }
-    //         Err(e) => {
-    //             println!("error: {}", e);
-    //         }
-    //     }
-    // }
+    // In this stage, you'll implement a TCP server that listens on port 6379.
+    let listener = TcpListener::bind("127.0.0.1:6379").expect("Listen to 6379 ports");
+    for stream in listener.incoming() {
+        match stream {
+            Ok(_stream) => {
+                println!("Accepting connection");
+            }
+            Err(_e) => {
+                println!("Error");
+            }
+        }
+    }
 }
