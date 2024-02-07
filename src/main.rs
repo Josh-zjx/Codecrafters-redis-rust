273994f16ddb49f1d8e963f4f96c69f4144c7f6b
Jiaxu Zhu <Josh Zhu>
2024-02-07 11:07:55 +0800

    multi connection

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,28 @@
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use std::thread;
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
-    // In this stage, you'll implement a TCP server that listens on port 6379.
+
     let listener = TcpListener::bind("127.0.0.1:6379").expect("Listen to 6379 ports");
+    let mut thread_handles = vec![];
+
     for stream in listener.incoming() {
         match stream {
-            Ok(stream) => handle_client(stream),
+            Ok(stream) => {
+                let handler = thread::spawn(move || {
+                    handle_client(stream);
+                });
+                thread_handles.push(handler);
+            }
             Err(_e) => {
                 println!("Error");
             }
         }
+    }
+    for handler in thread_handles {
+        let _ = handler.join();
     }
 }
 
@@ -20,8 +30,6 @@ fn handle_client(mut stream: TcpStream) {
     let mut read_buf: [u8; 256] = [0; 256];
     loop {
         let _read_result = stream.read(&mut read_buf).expect("Cannot read from stream");
-        let _write_result = stream
-            .write_all(b"+PONG\r\n")
-            .expect("Cannot write to the stream");
+        let _write_result = stream.write_all(b"+PONG\r\n");
     }
 }
